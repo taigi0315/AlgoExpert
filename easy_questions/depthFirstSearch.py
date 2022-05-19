94acc4d1dc4d09c686a5a0c8983273f351260117
taigi0315 <changik0315@gmail.com>
2022-05-19 12:34:22 -0500

    depth-first-search

---
+++ easy_questions/depthFirstSearch.py
@@ -0,0 +1,23 @@
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # init result with empty list
+        array.append(self.name)
+        if not self.children:
+            return
+        for c in self.children:
+            c.depthFirstSearch(array)
+		
+        return array
+
